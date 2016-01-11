03cf4eeac441a56103791c3af5486baba3bd0080
mEss1anh <dima-koval102496@rambler.ru>
2016-01-11 14:20:19 +0300

    New Class Shop

---
+++ Seminar/Shop.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Seminar
+{
+    class Shop
+    {
+        public int Id { get; set; }
+        public string SName { get; set; }
+    }
+}
