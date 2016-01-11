94dbf6288b11a8300c5fc364486192716331c38e
mEss1anh <dima-koval102496@rambler.ru>
2016-01-11 14:34:13 +0300

    New Entities class

---
+++ Seminar/Entities.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Seminar
+{
+    class Entities
+    {
+        public Shop Shops { get; set; }
+        public Car Cars { get; set; }
+    }
+}
